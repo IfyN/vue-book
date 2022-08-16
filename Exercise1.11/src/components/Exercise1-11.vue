fa93d7b595c5ef707537e59b768469bfaa0d7b6d
IfyN <ifynwosu0@gmail.com>
2022-08-16 14:07:50 +0100

    return data with vue method

---
+++ Exercise1.11/src/components/Exercise1-11.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+
+    <h1>Returning Shopping Cart Data with Vue methods</h1>
+    <div>Cart({{ totalItems }}) {{ totalCost }} </div>
+    <ul>
+      <li v-for="n in 5" :key="n">
+        <a href="#" @click="addToCart(n)">Add {{ n }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      totalCost: 0,
+      totalItems: 0,
+    }
+  },
+
+  methods: {
+    addToCart(n) {
+      this.totalCost = this.totalCost + 1,
+        this.totalItems = this.totalItems + 1
+    },
+  }
+
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  padding-left: 0;
+}
+
+li {
+  display: block;
+  list-style: none;
+
+  +li {
+    margin-top: 10px;
+  }
+}
+
+a {
+  display: inline-block;
+  background: rgb(235, 50, 50);
+  border-radius: 10px;
+  font-size: 10px;
+  color: white;
+  padding: 5px 10px;
+  text-decoration: none;
+}
+</style>
