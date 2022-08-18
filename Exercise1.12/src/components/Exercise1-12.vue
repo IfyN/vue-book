d3c2b043e9a10fb71fe4f06f5e75e28133435801
IfyN <ifynwosu0@gmail.com>
2022-08-18 15:21:44 +0100

    control data with lifecycle hooks

---
+++ Exercise1.12/src/components/Exercise1-12.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1> Control Data with Vue Lifecycles hook</h1>
+    <ul>
+      <li v-for="(item, n) in lists" :key="n">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lists: ['Game of Thrones', "Billions", "Handmaids Tale", "Curb Your Enthusiasm", "Abbott Elementary", "This is Us", "Emily in Paris"]
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  padding-left: 0;
+}
+
+li {
+  display: block;
+  list-style: none;
+
+  +li {
+    margin-top: 10px;
+  }
+}
+
+a {
+  display: inline-block;
+  background: rgb(235, 50, 50);
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-size: 10px;
+  color: white;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+</style>
