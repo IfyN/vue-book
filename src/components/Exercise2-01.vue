ac37734517191c614945400a8db53384e9450378
IfyN <ifynwosu0@gmail.com>
2022-09-23 18:44:42 +0100

    reactive computed props

--- src/components/Exercise2-01.vue
+++ src/components/Exercise2-01.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <input v-model="firstName" placeholder="First name" />
+    <input v-model="firstName" placeholder="First Name" />
     <input v-model="lastName" placeholder="Last name" />
 
     <h3 class="output">{{ fullName }}</h3>
@@ -18,7 +18,7 @@ export default {
   computed: {
     fullName() {
       return `${this.firstName} ${this.lastName}`
-    },
+    }
   },
 }
 </script>
@@ -29,10 +29,12 @@ export default {
   padding: 30px;
   max-width: 600px;
 }
+
 input {
   padding: 10px 6px;
   margin: 20px 10px 10px 0;
 }
+
 .output {
   font-size: 16px;
 }
